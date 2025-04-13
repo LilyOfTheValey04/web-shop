0f41b2ae3fec47966737990a391cb83dc8450e13
unknown <lilymaria044@gmailcom>
2025-04-13 20:51:59 +0300

    change in order prodect models

--- models/Order.js
+++ models/Order.js
@@ -1,31 +1,56 @@
 const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
-    productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product',required: true },
+    productId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Product',
+        required: true 
+    },
     quantity: {
         type: Number,
         required: true,
-        min: 1 // Минимално количество
+        min: 1
     },
-    customerName: {
+    // Лични данни (съхранени в поръчката за историческа точност)
+    firstName: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
-   
-  price: {
-         type: mongoose.Types.Decimal128,
-           required: true
-        },
-        customer: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    address: {
+        street: {
+            type: String,
             required: true
         },
-        purchaseDate: {
-            type: Date,
-            default: Date.now
+        city: {
+            type: String,
+            required: true
+        },
+        postalCode: {
+            type: String,
+            required: true
         }
-    }, { timestamps: true });
-
+    },
+    // Референция към потребител (ако има регистрация)
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: false // Незадължително за гости
+    },
+    // Цената на продукта в момента на поръчката
+    price: {
+        type: mongoose.Types.Decimal128,
+        required: true
+    },
+    purchaseDate: {
+        type: Date,
+        default: Date.now
+    }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Order', OrderSchema);
--- models/Product.js
+++ models/Product.js
@@ -1,11 +1,68 @@
 const mongoose = require('mongoose');
+const { Decimal128 } = mongoose.Types; // Изисква се явен импорт
 
 const ProductSchema = new mongoose.Schema({
-    name:String,
-    description:String,
-    price:Number,
-    image:String,
-    stock:Number,
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    stock: {
+        type: Number,
+        required: true,
+        min: 0,
+        validate: {
+            validator: Number.isInteger,
+            message: 'Stock must be an integer'
+        }
+    },
+    price: { 
+        type: Decimal128, 
+        required: true,
+        get: v => parseFloat(v.toString()), // Конвертиране към число
+        set: v => Decimal128.fromString(v.toFixed(2)) // Запазва 2 дес. знака
+    },
+    image: { 
+        type: String, 
+        required: true,
+        match: /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i // Валидация за URL
+    },
+    shortDescription: { 
+        type: String, 
+        required: true,
+        maxlength: 160 
+    },
+    fullDescription: { 
+        type: String, 
+        required: true 
+    },
+    reviews: [{
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        comment: {
+            type: String,
+            maxlength: 500
+        },    
+    
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }]
+}, { 
+    timestamps: true,
+    toJSON: { 
+        getters: true,
+        virtuals: true 
+    },
+    toObject: { 
+        getters: true,
+        virtuals: true 
+    }
 });
-
-module.exports = mongoose.model('Product', ProductSchema);
