4c051dfb887b02f7c45c759a69f6fc0ebe061aa6
unknown <lilymaria044@gmailcom>
2025-06-02 16:19:22 +0300

    working on orderform

--- routes/orders.js
+++ routes/orders.js
@@ -1,50 +1,16 @@
 const express= require('express');
 const router = express.Router();
-const Order = require('../models/Order');
-const Product = require('../models/Product');
+const path = require('path');
+const orderController = require('./controllers/orderController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Създаване на нова поръчка
-router.post('/',authMiddleware,async(req,res)=>{
-    try{ 
-        const product = await Product.findById(req.body.productId);
-        if (!product){
-            return res.status(404).json({message: 'Product not found'}); // Проверка дали продуктът съществува
-        }
 
-        if(product.stock<req.body.quantity){
-            return res.status(400).json({message: 'Not enough stock'}); // Проверка за наличност
-        }
+//not implemented
+//router.post('/',authMiddleware, orderController.createOrder);
+//router.get('/',authMiddleware, orderController.getOrder);
 
-    const totalPrice = parseFloat(product.price.toString()) * req.body.quantity; // Изчислява цената на поръчката
+router.post('/', orderController.createOrder);
+router.get('/', orderController.getOrder);
 
-    const newOrder = new Order({
-        ...req.body, // Взима данните от заявката
-        price:totalPrice// Добавя цената
-        });
-    product.stock -= req.body.quantity; // Намалява наличността на продукта
-    await newOrder.save()
-    res.status(201).json(newOrder);
-
-}catch(err){
-    res.status(500).json({message:err.message});
-}
-});
-
-// Получаване на всички поръчки
-router.get('/',authMiddleware,async (req, res) => {
-    try {
-        const orders = await Order.find()
-            .populate('productId')    // Продукти
-            .populate('customer');   // Потребители
-
-        res.json({ 
-            orders,  // Връща поръчките
-            message: 'Here are the orders' 
-        });
-    } catch (err) {
-        res.status(500).json({ message: "Server error" }); 
-    }
-});
 
 module.exports= router;
